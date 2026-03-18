3aa9bbc776ec150404f4aed7648125a7ba58c013
JChiusolo <jasonchiusolo@gmail.com>
2026-03-18 09:11:10 -0400

    Create main entry point for the React app serving microfrontends

---
+++ packages/app-shell/src/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Main entry point for the application shell
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
