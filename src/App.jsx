a788b854693df72b8511412f1a1aca2d2b8814aa
JChiusolo <jasonchiusolo@gmail.com>
2026-03-21 10:44:11 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import AuthCallback from './pages/AuthCallback'
-// Import your other pages/components here
-import YourMainPage from './pages/YourMainPage' // Replace with your actual main page
+import SearchPage from './pages/SearchPage'
+import HomePage from './pages/HomePage'
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/auth/callback" element={<AuthCallback />} />
-        <Route path="/" element={<YourMainPage />} />
-        {/* Add your other routes here */}
+        <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
       </Routes>
     </Router>
   )
