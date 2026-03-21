685bff9f20713a4516ea37fcec54bce50fcf5e94
JChiusolo <jasonchiusolo@gmail.com>
2026-03-21 10:22:21 -0400

    Create AuthCallback.jsx

---
+++ src/pages/AuthCallback.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+export default function AuthCallback() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const hash = window.location.hash
+    if (hash) {
+      const params = new URLSearchParams(hash.substring(1))
+      const accessToken = params.get('access_token')
+      
+      if (accessToken) {
+        localStorage.setItem('google_access_token', accessToken)
+        navigate('/')
+      }
+    }
+  }, [navigate])
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        <p className="text-lg font-semibold">Signing in...</p>
+      </div>
+    </div>
+  )
+}
